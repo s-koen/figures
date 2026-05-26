4bfbb3a4115637fb5d3ce75d67d2dfe049252527
kessers <koen.essers@ru.nl>
2026-05-26 12:42:17 +0200

    initial commit

---
+++ scripts/test.py
@@ -0,0 +1,20 @@
+# hello_plot.py
+
+import numpy as np
+import plotly.graph_objects as go
+
+x = np.linspace(0, 10, 1000)
+y = np.sin(x)
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=x, y=y, mode="lines", name="sin(x)"))
+
+fig.update_layout(
+    title="interactive sine wave",
+    xaxis_title="x",
+    yaxis_title="sin(x)",
+)
+
+fig.write_html("index.html", include_plotlyjs="cdn")
+# %%
