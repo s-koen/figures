9c932c87269ff31a1e78f13ceab56cdb4c4a8f46
kessers <koen.essers@ru.nl>
2026-05-26 16:34:03 +0200

    added figure

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,6 @@
 document.getElementById("sidebar").innerHTML = `<details><summary>master-internship</summary>
 </details>
 <details><summary>simple-test</summary>
+    <div><a target="viewer" href="plots/simple-test/interactive-sine-wave.html">interactive-sine-wave</a></div>
 </details>
 `;
