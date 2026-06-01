61d6903118c5d209fce1ab35316b5fc843a9417b
kessers <koen.essers@ru.nl>
2026-06-01 23:48:16 +0200

    added figures

--- sidebar.js
+++ sidebar.js
@@ -4,6 +4,11 @@ document.getElementById("sidebar").innerHTML = `<details><summary>master-interns
         <div><a target="viewer" href="#/plots/master-internship/w17/fit-surface-max">fit-surface-max</a></div>
         <div><a target="viewer" href="#/plots/master-internship/w17/fit-surface-none">fit-surface-none</a></div>
     </details>
+    <details><summary>w18</summary>
+        <div><a target="viewer" href="#/plots/master-internship/w18/fit-surface-low">fit-surface-low</a></div>
+        <div><a target="viewer" href="#/plots/master-internship/w18/fit-surface-max">fit-surface-max</a></div>
+        <div><a target="viewer" href="#/plots/master-internship/w18/fit-surface-none">fit-surface-none</a></div>
+    </details>
 </details>
 <details><summary>simple-test</summary>
     <div><a target="viewer" href="#/plots/simple-test/interactive-sine-wave">interactive-sine-wave</a></div>
